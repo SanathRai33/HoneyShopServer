e2217f0bf5736135d9a56bd8ac816134fa01b2b2
SanathRai <sanathrai03@gmail.com>
2025-10-15 23:46:02 +0530

    App and Database setup

--- server.js
+++ server.js
@@ -1,4 +1,8 @@
 require("dotenv").config();
+const app = require('./src/app.js')
+const connectDB = require('./src/db/db.js')
+
+connectDB()
 
 app.listen(process.env.PORT, () =>{
     console.log(`Server running on ${process.env.PORT}`)
