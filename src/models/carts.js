605c4660d0f1a555fa0d0e7137e9934ea1138a12
SanathRai <sanathrai03@gmail.com>
2025-10-16 21:16:53 +0530

    Create/Cart (Created a carts model)

---
+++ src/models/carts.js
@@ -0,0 +1,47 @@
+const mongoose = require("mongoose");
+
+const cartSchema = new mongoose.Schema(
+  {
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+      unique: true,
+    },
+    items: [{
+      product: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Product",
+        required: true,
+      },
+      quantity: {
+        type: Number,
+        required: true,
+        min: 1,
+        default: 1,
+      },
+      price: {
+        type: Number,
+        required: true,
+      },
+      addedAt: {
+        type: Date,
+        default: Date.now,
+      },
+    }],
+    totalItems: {
+      type: Number,
+      default: 0,
+    },
+    totalPrice: {
+      type: Number,
+      default: 0,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const Cart = mongoose.model("Cart", cartSchema);
+module.exports = Cart;
