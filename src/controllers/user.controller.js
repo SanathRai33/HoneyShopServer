4a8dedd0eb294a00dd472423cc27fff2d3694479
SanathRai <sanathrai03@gmail.com>
2025-10-17 23:45:59 +0530

    Test/User Controller & Router (Created user router and controller for update and get profile)

---
+++ src/controllers/user.controller.js
@@ -0,0 +1,57 @@
+const userModel = require('../models/users.model.js')
+
+async function userProfile(req, res) {
+  try {
+    const user = req.user;
+
+    if (!user) {
+      return res.status(401).json({ message: "User not found" });
+    }
+
+    return res.status(200).json({
+      user: {
+        id: user._id,
+        fullName: user.fullName,
+        email: user.email,
+        address: user?.address || "",
+      },
+      message: "User profile fetched successfully",
+    });
+  } catch (error) {
+    return res.status(500).json({ message: "Something went wrong", error });
+  }
+}
+
+async function updateUserProfile(req, res) {
+  try {
+    const { fullName, phone, address } = req.body;
+
+    const userId = req.user._id;
+
+    if (!userId) {
+      return res.status(404).json({ message: "User not found" });
+    }
+
+    const updatedUser = await userModel.findByIdAndUpdate(
+      userId,
+      { fullName, phone, address },
+      { new: true, runValidators: true }
+    );
+
+    if (!updatedUser) {
+      return res.status(404).json({ message: "User not found" });
+    }
+
+    return res.status(200).json({
+      message: "Profile updated successfully",
+      user: updatedUser,
+    });
+  } catch (error) {
+    return res.status(500).json({ message: "Something went wrong", error });
+  }
+}
+
+module.exports = {
+  userProfile,
+  updateUserProfile
+};
---
+++ src/routes/user.route.js
@@ -0,0 +1,9 @@
+const express = require('express')
+const router = express.Router();
+const { getUserProfile, updateUserProfile } = require('../controllers/user.controller.js')
+const { authUserMiddleware } = require('../middlewares/auth.middleware.js')
+
+router.get('/', authUserMiddleware, getUserProfile)
+router.patch('/', authUserMiddleware, updateUserProfile)
+
+module.exports = router;
