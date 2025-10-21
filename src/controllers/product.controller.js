94678e59c8760eb30396a3266317433d44d52c3c
SanathRai <sanathrai03@gmail.com>
2025-10-21 15:40:42 +0530

    Developed/Product Router&Controller (Controller and Router for Get vendor product)

--- src/controllers/product.controller.js
+++ src/controllers/product.controller.js
@@ -139,5 +139,38 @@ const getAllProduct = async (req, res) => {
   }
 };
 
+const getProductByVendorId = async (req, res) => {
+  try {
+    const vendorId = req.vendor._id;
 
-module.exports = { createProduct, getAllProduct };
+    const vendorExist = await vendorModel.findById({ vendorId });
+
+    if (!vendorExist) {
+      return res.status(200).json({
+        message: "Vendor not found, Login and Try again...",
+      });
+    }
+
+    const vendorProduct = await productModel.find({ vendor: vendorId });
+
+    if (!vendorProduct) {
+      return res.status(200).json({
+        message: "No products listed yet",
+      });
+    }
+
+    return res.status(200).json({
+      message: "Products fetched successfully",
+      products: vendorProduct,
+    });
+  } catch (error) {
+    console.error("Product creation error:", error);
+
+    return res.status(500).json({
+      error: error,
+      message: "Something went wrong",
+    });
+  }
+};
+
+module.exports = { createProduct, getAllProduct, getProductByVendorId };
--- src/routes/product.route.js
+++ src/routes/product.route.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const router = express.Router();
-const { createProduct, getAllProduct } = require('../controllers/product.controller.js')
+const { createProduct, getAllProduct, getProductByVendorId } = require('../controllers/product.controller.js')
 const { authAdminMiddleware, authVendorMiddleware } = require('../middlewares/auth.middleware.js')
 const { uploadProduct } = require('../config/cloudinary.js')
 
 router.post('/admin', authAdminMiddleware, uploadProduct, createProduct)
 router.post('/vendor', authVendorMiddleware, uploadProduct, createProduct)
 router.get('/', authAdminMiddleware, getAllProduct)
+router.get('/:id', authVendorMiddleware, getProductByVendorId)
 
 module.exports = router;
