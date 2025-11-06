9ec6227104913a9c746cbf4bc333841a3fb48501
SanathRai <sanathrai03@gmail.com>
2025-11-06 23:11:51 +0530

    Implement/UserController (Updated user controller for deleting user data)

--- src/routes/user.route.js
+++ src/routes/user.route.js
@@ -5,5 +5,6 @@ const { authUserMiddleware } = require('../middlewares/auth.middleware.js')
 
 router.get('/', authUserMiddleware, getUserProfile)
 router.patch('/', authUserMiddleware, updateUserProfile)
+router.delete('/', authUserMiddleware, deleteUserAccount)
 
 module.exports = router;
