2d06a8f5a2e63b68dd146e5b8ea1e6a8f190aa8b
SanathRai <sanathrai03@gmail.com>
2025-12-18 08:21:59 +0530

    Update/API (Updated base url)

--- src/app.js
+++ src/app.js
@@ -23,7 +23,7 @@ app.use(helmet({
 }));
 
 app.use(cors({
-  origin: process.env.FRONTEND_API_BASE_URL,
+  origin: process.env.BACKEND_SERVER_URL,
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
   allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With']
